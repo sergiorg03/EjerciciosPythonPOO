5886c2d07009d65e9890bd9fb4f35b3dc828fee5
Sergiorg03 <sergiorodriguezprofesional@gmail.com>
2025-12-03 17:01:41 +0100

    REalizado el ejercicio de prueba de conexion con BD MySQL en AIVEN

--- PruebaConexionMySQL_AIVEN.py
+++ PruebaConexionMySQL_AIVEN.py
@@ -1,17 +1,29 @@
 # Importamos las librerias necesarias
-import mysql.connector
-from .env import * as v
+# pip install pymysql, python-dotenv, cryptography
+import pymysql
+import cryptography
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Conexion con la base de datos
 try:    
-    conexion = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password=v.DB_PASSWORD,
-        database="defaultdb"
+    timeout = 10
+    conexion = pymysql.connect(
+        host="mysql-pruebas-sergiorodriguezprofesional-8ed7.h.aivencloud.com",
+        port=13648,
+        user="avnadmin",
+        password=os.getenv("DB_PASSWORD"),
+        db="defaultdb",
+        connect_timeout=timeout
     )
+
+    print("Conexión establecida correctamente. ")
+    '''cursor = connection.cursor()
+    cursor.execute("CREATE TABLE mytest (id INTEGER PRIMARY KEY)")
+    cursor.execute("INSERT INTO mytest (id) VALUES (1), (2)")
+    cursor.execute("SELECT * FROM mytest")
+    print(cursor.fetchall())'''
 except Exception as e:
     print(f"Error al conectar a la base de datos: {e}")
-finally:
-    if conexion:
-        conexion.close()
