560018f2bb50a51c584a2a8504f84809a0c50b45
Sergiorg03 <sergiorodriguezprofesional@gmail.com>
2026-01-01 22:16:57 +0100

    Cambio de color del boton para agregar libros y de los botones de solicitud y prestamo de libros

--- PRUEBAS.py
+++ PRUEBAS.py
@@ -1,10 +1,16 @@
 import re
 patron = re.compile('a[3-5]+') # coincide con una letra, seguida de al menos 1 dígito entre 3 y 5
 cadena = 'a44453'
-print(patron.match(cadena)) # <re.Match object; span=(0, 6), match='a44453'>
-print(patron.search(cadena)) # <re.Match object; span=(0, 6), match='a44453'>
+#print(patron.match(cadena)) # <re.Match object; span=(0, 6), match='a44453'>
+#print(patron.search(cadena)) # <re.Match object; span=(0, 6), match='a44453'>
 cadena = 'ba3455' # la coincidencia no está al principio!
-print(patron.search(cadena)) # <re.Match object; span=(1, 6), match='a3455'>
-print(patron.match(cadena)) # None
+#print(patron.search(cadena)) # <re.Match object; span=(1, 6), match='a3455'>
+#print(patron.match(cadena)) # None
 
-print(isinstance())
+
+def triangulo(num):
+    for i in range(num):
+        print(" "*(num - i - 1) + "*" * (2 * i + 1))
+
+triangulo(4)
+
