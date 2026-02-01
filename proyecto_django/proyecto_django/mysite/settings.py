68bbc46474b33e454fdfc6bfd06f073a6bd20282
sergiorg03 <sergiorodriguezprofesional@gmail.com>
2026-02-01 17:51:23 +0100

    Realizadas modificaciones para la subida de la app al servidor

--- proyecto_django/proyecto_django/mysite/settings.py
+++ proyecto_django/proyecto_django/mysite/settings.py
@@ -32,9 +32,13 @@ print(BASE_DIR)
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    ".onrender.com",
+]
 
 
 # Application definition
